44bb928feedec7fa5fcbe6233df0cbd5fd5e521f
Sidd2024 <sid162002@gmail.com>
2022-12-25 13:10:31 +0530

    added models and updated urls

--- dlai_task/pdfScan/models.py
+++ dlai_task/pdfScan/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    pass
+
+class Text(models.Model):
+    text = models.TextField(default="")
+    filename = models.CharField(max_length=64)
